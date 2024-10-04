cb8383c7087b718ac1e2655a94dcd2d448a093d8
mohamed elquesni <mohamedelquesni@gmail.com>
2024-10-04 22:34:38 +0300

    Update- Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,5 +25,31 @@ const addProduct = async (req, res) => {
     res.status(500).json({ message: "Error adding product" });
   }
 };
+const updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id; 
+    const { name, description, price, quantity, status } = req.body; 
 
-module.exports = { addProduct };
+    // Find the product by ID and update it
+    const updatedProduct = await productModel.findByIdAndUpdate(
+      productId,
+      { name, description, price, quantity, status },
+      { new: true, runValidators: true } 
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    
+    res.status(200).json({
+      message: "Product updated successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Error updating product" });
+  }
+};
+
+
+module.exports = { addProduct, updateProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,4 +4,5 @@ const productController = require("../controllers/productController");
 const router = express.Router();
 
 router.post("/add-product/:id", productController.addProduct);
+router.put("/edit-product/:id", productController.updateProduct);
 module.exports = router;
