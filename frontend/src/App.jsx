40a7dde25356bec086b9cb178fd1428fed644f8f
nourahmed109 <nourahmed_2003@yahoo.com>
2024-10-05 22:57:22 +0300

    deleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ProductCard from './components/Cards/ProductCard.jsx';  // Import the ProductCard component
+import React , {useState } from 'react';
 import './App.css';
+import MainRoutes from "./Routes.jsx"
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      {/* Map over the products array to display multiple ProductCard components */}
-      {products.map((product, index) => (
-        <ProductCard key={index} product={product} />
-      ))}
+    <div>
+    <MainRoutes/>
+
     </div>
   );
 }
