511efb5b57b19d6440cf6902d98b6aef8eb91176
mohamed elquesni <mohamedelquesni@gmail.com>
2024-10-08 03:32:13 +0300

    Req 9 tested

--- frontend/src/components/Seller/SellerPageBody.jsx
+++ frontend/src/components/Seller/SellerPageBody.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SellerProducts from "./SellerProducts/SellerProducts";
 import AdminProducts from "../Admin/AdminProducts/AdminProducts";
-
+import CreateSellerProfile from "./CreateSellerProfile";
+import EditSellerProfile from "./EditSellerProfile";
 export const SellerPageBody = () => {
   return (
     <div className="flex-1 p-5">
       <Routes>
         <Route path="products" element={<AdminProducts />} />
-      </Routes>
+        <Route path="account/create/:id" element={<CreateSellerProfile />} />
+        <Route path="account/edit" element={<EditSellerProfile />} />
+
+        </Routes>
     </div>
   );
 };
