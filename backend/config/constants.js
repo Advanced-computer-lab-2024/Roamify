0463df0cfe3d8499316ac91a5fd050a56144cb58
mohamed elquesni <mohamedelquesni@gmail.com>
2024-11-12 22:27:13 +0200

    Adjusted Itinerary --> search name

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -4,5 +4,5 @@
     module.exports = {
       PORT: process.env.PORT || 3000,
       DB_URI: process.env.DB_URI,
-      CORS_ORIGIN: "http://localhost:5175",
+      CORS_ORIGIN: "http://localhost:5173",
     };
