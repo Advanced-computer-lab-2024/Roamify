114d36f488336cda0fdc53132120694eb3db5057
Farah Hesham Ahmed <farahhesham04@icloud.com>
2024-12-08 16:10:18 +0200

    activities

--- backend/controllers/resetPasswordController.js
+++ backend/controllers/resetPasswordController.js
@@ -97,7 +97,7 @@ const checkOtp = async (req, res) => {
     } else {
       return res.status(400).json({ message: "Invalid OTP" });
     }
-    catch (error) {
+   } catch (error) {
         return res.status(500).json({ message: error.message })
     }
 }
