af768f952faa2d214f1085e67c790bf86fa05b44
nourahmed109 <nourahmed_2003@yahoo.com>
2024-12-12 01:26:20 +0200

    ii

--- frontend/src/page/Register.js
+++ frontend/src/page/Register.js
@@ -1,6 +1,4 @@
 import React from 'react'
-// import CommonBanner
-import CommonBanner from '../component/Common/CommonBanner'
 // Import RegisterArea
 import RegisterArea from '../component/Register'
 
@@ -8,7 +6,6 @@ import RegisterArea from '../component/Register'
 const Register = () => {
   return (
     <>
-        <CommonBanner heading="Register" pagination="Register"/>
         <RegisterArea/>
     </>
   )
